fd4b4ec886730f9252992f0c6a0a9d04ff92598c
Michał Kiełtyka <defozo@gmail.com>
2024-09-18 15:18:24 +0200

    Changed maximum loop exceeded exception in the rewrite

--- agent/component/rewrite.py
+++ agent/component/rewrite.py
@@ -54,7 +54,7 @@ class RewriteQuestion(Generate, ABC):
             setattr(self, "_loop", 0)
         if self._loop >= self._param.loop:
             self._loop = 0
-            raise Exception("Sorry! Nothing relevant found.")
+            raise Exception("I don't have a good answer for this, I'm sorry.")
         self._loop += 1
         q = "Question: "
         for r, c in self._canvas.history[::-1]:
